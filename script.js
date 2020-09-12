ef2a3e5814561deed4fc450b9924a49fd1a8b836
Your <yourname@your-email.com>
2020-09-12 01:06:31 -0400

    jQuery partial commit

---
+++ script.js
@@ -0,0 +1,248 @@
+
+
+const skincareProducts = {
+  cleanser: [
+    {
+      title: 'iS Clinical Cleansing Complex',
+      price: '$$$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'acne', 'anti-aging'],
+      whereToBuy: 'https://www.theskingirls.ca/collections/is-clinical/products/cleansing-complex-180ml-1'
+    },
+
+    {
+      title: 'Tatcha The Deep Cleanse',
+      price: '$$',
+      skinType: ['oily', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'acne'],
+      whereToBuy: 'https://www.sephora.com/ca/en/product/the-deep-cleanse-P427536'
+    },
+
+    {
+      title: 'Peach & Lily Power Calm Hydrating Gel Cleanser',
+      price: '$$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'acne', 'anti-aging'],
+      whereToBuy: 'https://www.ulta.com/power-calm-hydrating-gel-cleanser?productId=pimprod2004468'
+    },
+
+    {
+      title: 'Glossier Milky Jelly Cleanser',
+      price: '$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'acne'],
+      whereToBuy: 'https://www.glossier.com/products/milky-jelly-cleanser'
+    },
+    
+    {
+      title: 'CeraVe Hydrating Facial Cleanser',
+      price: '$',
+      skinType: ['dry', 'sensitive', 'normal'],
+      skinGoal: ['brightening', 'acne', 'anti-aging'],
+      whereToBuy: 'https://beauty.shoppersdrugmart.ca/Beauty/Categories/Natural-Beauty/Skin-Care/Cleansers/Hydrating-Cleanser/p/BB_3606000531833?variantCode=3606000546349'
+    },
+
+    {
+      title: 'Cetaphil Gentle Skin Cleanser',
+      price: '$',
+      skinType: ['dry', 'sensitive', 'normal'],
+      skinGoal: ['brightening', 'acne', 'anti-aging'],
+      whereToBuy: 'https://www.walmart.ca/en/ip/cetaphil-gentle-skin-cleanser/6000188115636'
+    }
+  ],
+
+  serumOrTreatment: [
+    {
+      title: 'SkinCeuticals C E Ferulic',
+      price: '$$$',
+      skinType: ['dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'aging'],
+      whereToBuy: 'https://www.dermstore.com/product_C+E+Ferulic_5230.htm',
+    },
+
+    {
+      title: 'Squalane + Phyto-Retinol Serum',
+      price: '$$$',
+      skinType: ['oily', 'dry', 'combination', 'normal'],
+      skinGoal: 'anti-aging',
+      whereToBuy: 'https://www.sephora.com/product/squalane-phyto-retinol-serum-P429028',
+    },
+
+    {
+      title: 'Caudalie Vinopure Natural Salicylic Acid Pore Minimizing Serum',
+      price: '$$$',
+      skinType: ['oily', 'combination'],
+      skinGoal: 'acne',
+      whereToBuy: 'https://www.sephora.com/ca/en/product/vinopure-natural-salicylic-acid-pore-minimizing-serum-P433626'
+    },
+
+    {
+      title: 'Dr. Jart+ Cicapair Tiger Grass Serum',
+      price: '$$',
+      skinType: ['oily', 'dry', 'combination', 'normal'],
+      skinGoal: 'acne',
+      whereToBuy: 'https://www.sephora.com/product/tiger-grass-re-pair-serum-P423259',
+    },
+
+    {
+      title: 'Klairs - Freshly Juiced Vitamin Drop',
+      price: '$$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'anti-aging'],
+      whereToBuy: 'https://www.mikaela-beauty.com/products/klairs-freshly-juiced-vitamin-drop',
+    },
+
+    {
+      title: 'The Ordinary Alpha Arbutin 2% + HA',
+      price: '$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: 'brightening',
+      whereToBuy: 'https://www.sephora.com/ca/en/product/the-ordinary-deciem-alpha-arbutin-2-ha-P427412',
+    },
+
+    {
+      title: 'The INKEY List Q10 Antioxidant Serum',
+      price: '$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: 'anti-aging',
+      whereToBuy: 'https://www.theinkeylist.com/products/q10',
+    },
+
+    {
+      title: 'The Ordinary Niacinamide 10% + Zinc 1%',
+      price: '$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'acne'],
+      whereToBuy: 'https://theordinary.deciem.com/product/rdn-niacinamide-10pct-zinc-1pct-30ml?ccvis=1',
+    },
+  ],
+
+  faceCream: [
+    {
+      title: 'SK-II R.N.A. POWER Anti-Aging Face Cream',
+      price: '$$$',
+      skinType: ['dry', 'sensitive', 'normal'],
+      skinGoal: 'anti-aging',
+      whereToBuy: 'https://www.sephora.com/product/r-n-a-power-radical-new-age-cream-P405950',
+    },
+
+    {
+      title: 'Drunk Elephant Protini Polypeptide Moisturizer',
+      price: '$$$',
+      skinType: ['oily', 'dry', 'combination', 'normal'],
+      skinGoal: ['brightening', 'acne', 'anti-aging'],
+      whereToBuy: 'https://www.sephora.com/ca/en/product/protini-tm-polypeptide-cream-P427421',
+    },
+
+    {
+      title: 'Then I Met You Calming Tide Gel Cream',
+      price: '$$',
+      skinType: ['oily', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'acne', 'anti-aging'],
+      whereToBuy: 'https://sokoglam.com/products/then-i-met-you-calming-tide-gel-cream',
+    },
+
+    {
+      title: 'Etude House Moistfull Collagen Cream',
+      price: '$$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: ['brightening', 'anti-aging'],
+      whereToBuy: 'https://sukoshimart.com/products/etude-house-moistfull-collagen-cream',
+    },
+
+    {
+      title: 'CeraVe Moisturizing Cream',
+      price: '$',
+      skinType: ['oily', 'dry', 'sensitive', 'combination', 'normal'],
+      skinGoal: 'brightening',
+      whereToBuy: 'https://www.walmart.ca/en/ip/cerave-moisturizing-cream/6000105907280',
+    },
+
+    {
+      title: 'Neutrogena Hydro Boost Facial Gel Cream with Hyaluronic Acid',
+      price: '$',
+      skinType: 'dry',
+      skinGoal: ['brightening', 'acne', 'anti-aging'],
+      whereToBuy: 'https://www.walmart.ca/en/ip/neutrogena-hydro-boost-facial-gel-cream-with-hyaluronic-acid/6000192034944',
+    },
+
+    {
+      title: 'Cetaphil Daily Hydrating Lotion',
+      price: '$',
+      skinType: 'sensitive',
+      skinGoal: ['brightening', 'acne'],
+      whereToBuy: 'https://www.walmart.ca/en/ip/cetaphil-daily-hydrating-lotion/6000198008661',
+    },
+  ]
+};
+
+const skincareApp = {};
+
+// PSEUDO CODE:
+
+// If there is more than one skincare product based on user's input, then create a random selector for which product will result
+function randomProduct(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+// Create an event listener for a 'submit' event
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  // Saving user input (from checked radio buttons) into variables
+  // Skin Type (oily, dry, sensitive, combination, normal), Budget ($, $$, $$$), Skin Goal (brightening, acne, anti-aging)
+  const chosenProduct = $('input[name=product]:checked').val();
+  const skincareType = $('input[name=type]:checked').val();
+  const budgetToSpend = $('input[name=price]:checked').val();
+  const skincareGoal = $('input[name=goal]:checked').val();
+  // Determine correspondence of user input with options in object 'skincareProducts'
+    // E.g. Oily -> $$ -> acne = 'cleanser', 'serum/treatment', 'facial cream'
+  const products = skincareProducts[chosenProduct];
+  
+  const finalProducts = [];
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    if (budgetToSpend === product.price) {
+      finalProducts.push(product);
+    }
+  }
+  // Display randomly chosen product on page in 'skincareProduct' div
+  const displayProducts = randomProduct(finalProducts);
+  $('#skincareProduct').html(`
+  <h2>${displayProducts.title}</h2>
+  <a href='${displayProducts.whereToBuy}' target='_blank'>Purchase Product Here!</a>
+  `);
+});
+
+// Allow user to start quiz again
+// Error handling - ensure user selects all options before allowing submission
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+skincareApp.init = function(){
+
+}
+
+
+
+
+
+
+
+$(function(){
+  skincareApp.init();
+});
