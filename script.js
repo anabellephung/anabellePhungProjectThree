ca584c71b29b1ff25b4b5ddcd4ea0ebfe9a8867a
Your <yourname@your-email.com>
2020-09-15 11:47:13 -0400

    update

--- script.js
+++ script.js
@@ -534,7 +534,7 @@ function randomProduct(array) {
 const target = $(this).attr('button');
 const smoothScroll = function(target) {
   $('html, body').animate({
-    scrollTop: ($(target).offset().top -200)
+    scrollTop: ($(target).offset().top -105)
   }, 1500);
 }  
 
